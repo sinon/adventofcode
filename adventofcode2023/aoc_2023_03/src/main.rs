03a41430913c3a01d8e1841a9ff397813e1eda62
Rob Hand <146272+Sinon@users.noreply.github.com>
2023-12-03 13:50:08 +0000

    Part 2

--- adventofcode2023/aoc_2023_03/src/main.rs
+++ adventofcode2023/aoc_2023_03/src/main.rs
@@ -1,17 +1,21 @@
-use std::str::Lines;
+use std::{collections::HashMap, str::Lines};
 
 fn main() {
     let lines = include_str!("input.txt").lines();
     let answer1 = part_1(lines);
     println!("Part 1: {}", answer1);
+
+    let lines = include_str!("input.txt").lines();
+    let answer2 = part_2(lines);
+    println!("Part 2: {}", answer2);
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, Hash, PartialEq, Clone)]
 struct Point {
     row: usize,
     column: usize,
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct NumberRange {
     start: Point,
     end: Point,
@@ -19,7 +23,11 @@ struct NumberRange {
 }
 
 impl NumberRange {
-    fn has_nearby_symbol(&self, grid: &[Vec<char>]) -> bool {
+    fn has_nearby_symbol(
+        &self,
+        grid: &[Vec<char>],
+        check_fn: fn(char) -> bool,
+    ) -> (bool, Option<Point>) {
         // 467..114..
         // ...*......
         // ..35..633.
@@ -44,32 +52,60 @@ impl NumberRange {
 
         // Above Horizontal
         for col_idx in top_l.column..(top_r.column + 1).clamp(0, col_boundary) {
-            if is_symbol(grid[top_r.row][col_idx]) {
-                return true;
+            if check_fn(grid[top_r.row][col_idx]) {
+                return (
+                    true,
+                    Some(Point {
+                        row: top_r.row,
+                        column: col_idx,
+                    }),
+                );
             }
         }
         // Below Horizontal
         for col_idx in bottom_l.column..(bottom_r.column + 1).clamp(0, col_boundary) {
-            if is_symbol(grid[bottom_l.row][col_idx]) {
-                return true;
+            if check_fn(grid[bottom_l.row][col_idx]) {
+                return (
+                    true,
+                    Some(Point {
+                        row: bottom_l.row,
+                        column: col_idx,
+                    }),
+                );
             }
         }
 
         // Left Vertical
-        if is_symbol(grid[self.start.row][self.start.column.saturating_sub(1)]) {
-            return true;
+        if check_fn(grid[self.start.row][self.start.column.saturating_sub(1)]) {
+            return (
+                true,
+                Some(Point {
+                    row: self.start.row,
+                    column: self.start.column.saturating_sub(1),
+                }),
+            );
         }
         // Right Vertical
         let end_c = (self.end.column + 1).clamp(0, col_boundary);
-        if is_symbol(grid[self.end.row][end_c]) {
-            return true;
+        if check_fn(grid[self.end.row][end_c]) {
+            return (
+                true,
+                Some(Point {
+                    row: self.end.row,
+                    column: end_c,
+                }),
+            );
         }
-        false
+        (false, None)
     }
 }
 
 fn is_symbol(c: char) -> bool {
     c.is_ascii_punctuation() && c != '.'
+}
+
+fn is_gear(c: char) -> bool {
+    c == '*'
 }
 
 #[test]
@@ -90,7 +126,8 @@ fn test_has_nearby_symbol() {
     // let num_range = &number_ranges[0];
     // assert_eq!(num_range.has_nearby_symbol(&grid), true);
     let num_range = &number_ranges[1];
-    assert_eq!(num_range.has_nearby_symbol(&grid), false);
+    let (is_nearby, _) = num_range.has_nearby_symbol(&grid, is_symbol);
+    assert_eq!(is_nearby, false);
 }
 
 fn get_number_ranges_from_grid(grid: &[Vec<char>]) -> Vec<NumberRange> {
@@ -160,8 +197,35 @@ fn part_1(lines: Lines) -> usize {
     // Search the space around the range for a symbol
     let mut total = 0;
     for num_range in number_ranges {
-        if num_range.has_nearby_symbol(&grid) {
+        let (is_nearby, _) = num_range.has_nearby_symbol(&grid, is_symbol);
+        if is_nearby {
             total += num_range.value;
+        }
+    }
+    total
+}
+
+fn part_2(lines: Lines) -> usize {
+    // Load lines in to a 2D Array i.e x,y row/column
+    let grid = grid_from_lines(lines);
+    let number_ranges = get_number_ranges_from_grid(&grid);
+    let mut gear_range_mapping: HashMap<Point, Vec<usize>> = HashMap::new();
+    for num_range in number_ranges {
+        let (_, gear_loc) = num_range.has_nearby_symbol(&grid, is_gear);
+        if let Some(x) = gear_loc {
+            gear_range_mapping
+                .entry(x)
+                .and_modify(|i| i.push(num_range.value))
+                .or_insert(vec![num_range.value]);
+        }
+    }
+    let mut total = 0;
+    for (_, values) in gear_range_mapping.iter() {
+        if values.len() == 2 {
+            total += values[0] * values[1]
+        } else if values.len() == 1 {
+        } else {
+            unreachable!("Multiple gears")
         }
     }
     total
