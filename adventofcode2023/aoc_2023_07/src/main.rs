2feebd2faf673f71afd6580d84ec22b05327edc2
Rob Hand <146272+Sinon@users.noreply.github.com>
2023-12-09 13:21:41 +0000

    Part 2, test pass, failed submission

--- adventofcode2023/aoc_2023_07/src/main.rs
+++ adventofcode2023/aoc_2023_07/src/main.rs
@@ -70,8 +70,8 @@ impl PartialOrd for CamelHand {
 
 impl Ord for CamelHand {
     fn cmp(&self, other: &Self) -> Ordering {
-        let ht1 = self.classify();
-        let ht2 = other.classify();
+        let ht1 = self.classify_with_joker();
+        let ht2 = other.classify_with_joker();
         if ht1 != ht2 {
             return ht1.cmp(&ht2);
         } else {
@@ -98,9 +98,55 @@ impl CamelHand {
         CamelHand { cards, bet }
     }
 
+    fn classify_with_joker(&self) -> HandType {
+        let mut counter: BTreeMap<Rank, i64> = BTreeMap::new();
+        // Count the cards but filter out Jacks
+        counter = self.cards.iter().filter(|x| **x != Rank::Jack).fold(
+            counter,
+            |mut acc, &item| match acc.get(&item) {
+                Some(x) => {
+                    acc.insert(item, x + 1);
+                    acc
+                }
+                None => {
+                    acc.insert(item, 1);
+                    acc
+                }
+            },
+        );
+        let mut new_hand: Vec<i64> = counter.values().copied().collect();
+        let card_count = new_hand.iter().copied().sum::<i64>();
+        match card_count {
+            0 => {
+                new_hand = vec![5];
+            }
+            1..=4 => {
+                let new_hand_len = new_hand.len();
+                let missing_cards = 5 - card_count;
+                new_hand.sort();
+                new_hand[new_hand_len - 1] += missing_cards;
+            }
+            5 => new_hand.sort(),
+            _ => unreachable!(),
+        }
+
+        match new_hand.as_slice() {
+            [_, _, _, _, 1] => HandType::HighCard,
+            [_, 2, 2] => HandType::TwoPair,
+            [_, _, _, 2] => HandType::Pair,
+            [_, _, 3] => HandType::ThreeOfAKind,
+            [_, 3] => HandType::FullHouse,
+            [_, 4] => HandType::FourOfAKind,
+            [5] => HandType::FiveOfAKind,
+            _ => {
+                unreachable!("Invalid hand: {:?}", new_hand);
+            }
+        }
+    }
+
     fn classify(&self) -> HandType {
-        let counter: BTreeMap<Rank, i64> = BTreeMap::new();
-        let counter = self
+        let mut counter: BTreeMap<Rank, i64> = BTreeMap::new();
+        counter = self
             .cards
             .iter()
             .fold(counter, |mut acc, &item| match acc.get(&item) {
@@ -116,8 +162,8 @@ impl CamelHand {
         let mut x: Vec<&i64> = counter.values().collect();
         x.sort();
         match x.as_slice() {
-            [1, 1, 1, 1, 1] => HandType::HighCard,
-            [1, 1, 1, 2] => HandType::Pair,
+            [_, _, _, _, 1] => HandType::HighCard,
+            [_, _, _, 2] => HandType::Pair,
             [_, 2, 2] => HandType::TwoPair,
             [_, _, 3] => HandType::ThreeOfAKind,
             [_, 3] => HandType::FullHouse,
@@ -132,10 +178,13 @@ impl CamelHand {
 
 fn main() {
     let input = include_str!("./input.txt");
-    let p1_answer = p1(input);
-    println!("Part 1: {}", p1_answer);
+    // let p1_answer = p1(input);
+    // println!("Now wrong due to joker Part 1: {}", p1_answer);
+    let p2_answer = p2(input);
+    println!("Part 2: {}", p2_answer);
 }
 
+#[allow(dead_code)]
 fn p1(input: &str) -> i64 {
     let mut hands = input.lines().map(CamelHand::new).collect::<Vec<_>>();
     hands.sort();
@@ -153,7 +202,25 @@ fn p1(input: &str) -> i64 {
     total
 }
 
+fn p2(input: &str) -> i64 {
+    let mut hands = input.lines().map(CamelHand::new).collect::<Vec<_>>();
+    hands.sort();
+    let mut total = 0;
+    for (i, h) in hands.iter().enumerate() {
+        println!(
+            "Rank: {} {:?} {:?} {:?}",
+            i + 1,
+            h.cards,
+            h.bet,
+            h.classify_with_joker()
+        );
+        total += h.bet * (i as i64 + 1);
+    }
+    total
+}
+
 #[test]
+#[ignore = "Different ordering for p2"]
 fn test_p1() {
     let input = "32T3K 765
 T55J5 684
@@ -161,4 +228,14 @@ KK677 28
 KTJJT 220
 QQQJA 483";
     assert_eq!(p1(input), 6440);
+}
+
+#[test]
+fn test_p2() {
+    let input = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+    assert_eq!(p2(input), 5905);
 }
