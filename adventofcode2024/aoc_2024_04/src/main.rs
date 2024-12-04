fa69ee88c1b037ffe2f1b854dcf7ccf08a31f298
Rob Hand <146272+sinon@users.noreply.github.com>
2024-12-04 22:48:40 +0000

    2024 Day 4 Part 1

--- adventofcode2024/aoc_2024_04/src/main.rs
+++ adventofcode2024/aoc_2024_04/src/main.rs
@@ -1,3 +1,5 @@
+use std::process::id;
+
 fn main() {
     let input = include_str!("input.txt");
     let part1 = part_1(input);
@@ -17,54 +19,76 @@ where
         .collect()
 }
 
-fn slant_grid<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>>
-where
-    T: Clone + std::fmt::Debug,
-{
-    assert!(!v.is_empty());
-    // 1 2 3
-    // 4 5 6
-    // 7 8 9
-    // to
-    // 1 5 9
-    // 2 6
-    // 3
-    // 4 8
-    // 7
-
-    // (0, 0), (1, 1), (2, 2)
-    // (0, 1), (1, 2)
-    // (1, 0), (2, 1)
-    // (0, 3)
-    // (3, 0)
-    let max_diag_length = v.len() + v[0].len() - 1;
-    let mut slanted: Vec<Vec<T>> = Vec::new();
-    for i in 0..max_diag_length {
-        slanted.push(Vec::new());
+fn extract_diagonals_top_right<T: Clone>(grid: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+    if grid.is_empty() || grid[0].is_empty() {
+        return Vec::new();
     }
 
-    for r in 0..v.len() {
-        for c in 0..v[0].len() {
-            let i = v[r][c].clone();
-            slanted[r + c].push(v[r][c].clone());
+    let rows = grid.len();
+    let cols = grid[0].len();
+
+    let max_diag_length = rows + cols - 1;
+
+    let mut diagonals: Vec<Vec<T>> = (0..max_diag_length).map(|_| Vec::new()).collect();
+
+    for r in 0..rows {
+        for c in 0..cols {
+            diagonals[r + c].push(grid[r][c].clone());
         }
     }
 
-    slanted
+    diagonals
+}
+
+fn extract_diagonals_top_left<T: Clone>(grid: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+    if grid.is_empty() || grid[0].is_empty() {
+        return Vec::new();
+    }
+
+    let rows = grid.len();
+    let cols = grid[0].len();
+
+    let max_diag_length = rows + cols - 1;
+
+    let mut diagonals: Vec<Vec<T>> = (0..max_diag_length).map(|_| Vec::new()).collect();
+
+    for r in 0..rows as i32 {
+        for c in 0..cols as i32 {
+            let idx: usize = (c - r + (rows as i32 - 1)).try_into().unwrap();
+            diagonals[idx].push(grid[r as usize][c as usize].clone());
+        }
+    }
+
+    diagonals
 }
 
 fn part_1(input: &str) -> i32 {
     let grid: Vec<Vec<char>> = input.lines().map(|x| x.chars().collect()).collect();
     let grid2 = transpose(grid.clone());
-    let grid3 = slant_grid(grid.clone());
-    println!("{:?}", grid);
-    println!("{:?}", grid2);
+    let grid3 = extract_diagonals_top_right(&grid);
+    let grid4 = extract_diagonals_top_left(&grid);
+    println!("Grid1");
+    for g in &grid {
+        println!("{:?}", g)
+    }
+    println!("Grid2");
+    for g in &grid2 {
+        println!("{:?}", g)
+    }
+    println!("Grid3");
+    for g in &grid3 {
+        println!("{:?}", g)
+    }
+    println!("Grid4");
+    for g in &grid4 {
+        println!("{:?}", g)
+    }
 
     let mut sum = 0;
-    sum += count_words(grid);
-    sum += count_words(grid2);
-    sum += count_words(grid3);
-
+    sum += count_words(grid); // -->, <---
+    sum += count_words(grid2); // Up, Down
+    sum += count_words(grid3); // Diagonal Top Left to Bottom Right
+    sum += count_words(grid4); // Diagonal Top Left to Bottom Right
     sum as i32
 }
 
@@ -105,6 +129,48 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX";
     assert_eq!(part_1(test_input), 18);
+}
+#[test]
+fn test_part_1_1() {
+    let test_input = "XMAS
+M---
+A---
+S---";
+    assert_eq!(part_1(test_input), 2);
+}
+
+#[test]
+fn test_part_1_2() {
+    let test_input = "SAMX
+A---
+M---
+X---";
+    assert_eq!(part_1(test_input), 2);
+}
+
+#[test]
+fn test_part_1_3() {
+    let test_input = "X---
+-M--
+--A-
+---S";
+    assert_eq!(part_1(test_input), 1);
+}
+
+#[test]
+fn test_part_1_4() {
+    let test_input = "---X
+-MM-
+-AA-
+S--S";
+    assert_eq!(part_1(test_input), 1);
+}
+
+#[test]
+fn test_transpose() {
+    let t = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+    let expected = vec![vec![1, 4, 7], vec![2, 5, 8], vec![3, 6, 9]];
+    assert_eq!(transpose(t), expected);
 }
 
 #[test]
