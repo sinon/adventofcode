f5efa057d370e6a3e8b1e56fc9e6dacd93c0f5c5
Rob Hand <146272+sinon@users.noreply.github.com>
2025-03-18 21:04:55 +0000

    2019 - Day 1 - Part 1 (#20)

---
+++ 2019/day-01/src/part2.rs
@@ -0,0 +1,21 @@
+#[tracing::instrument]
+pub fn process(_input: &str) -> miette::Result<i32> {
+    Ok(_input
+        .lines()
+        .map(|s| {
+            let i = s.parse::<f32>().unwrap();
+            ((i / 3.0).floor() - 2.0) as i32
+        })
+        .sum())
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "";
+        assert_eq!(0, process(input)?);
+        Ok(())
+    }
+}
